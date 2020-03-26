d08c637faef723978c78b5a9820d3113455337ab
Scott Zhao <zhaomh1998@outlook.com>
2020-03-26 04:00:02 -0700

    basic tests

--- bot.py
+++ bot.py
@@ -5,7 +5,103 @@ from slack import WebClient
 from slackeventsapi import SlackEventAdapter
 import ssl as ssl_lib
 import certifi
-from onboarding_tutorial import OnboardingTutorial
+
+
+def get_app_home():
+    return {
+        "type": "home",
+        "blocks": [
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "Hi David :wave:"
+                }
+            },
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "Great to see you here! App helps you to stay up-to-date with your meetings and events right here within Slack. These are just a few things which you will be able to do:"
+                }
+            },
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "• Schedule meetings \n • Manage and update attendees \n • Get notified about changes of your meetings"
+                }
+            },
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "But before you can do all these amazing things, we need you to connect your calendar to App. Simply click the button below:"
+                }
+            },
+            {
+                "type": "actions",
+                "elements": [
+                    {
+                        "type": "button",
+                        "text": {
+                            "type": "plain_text",
+                            "text": "Connect to a Tutor / TA",
+                            "emoji": True
+                        },
+                        "value": "click_me_123"
+                    }
+                ]
+            }
+        ]
+    }
+
+
+def get_request_block():
+    return [
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "You have a new request from <studentName>:\n*<fakeLink.toEmployeeProfile.com|Click to chat with studentName>*"
+                }
+            },
+            {
+                "type": "section",
+                "fields": [
+                    {
+                        "type": "mrkdwn",
+                        "text": "*Question Brief:*\n<question brief>"
+                    }
+                ]
+            },
+            {
+                "type": "actions",
+                "elements": [
+                    {
+                        "type": "button",
+                        "text": {
+                            "type": "plain_text",
+                            "emoji": True,
+                            "text": "Finished!"
+                        },
+                        "style": "primary",
+                        "value": "click_me_123"
+                    },
+                    {
+                        "type": "button",
+                        "text": {
+                            "type": "plain_text",
+                            "emoji": True,
+                            "text": "Pass to Other Tutor"
+                        },
+                        "style": "danger",
+                        "value": "click_me_123"
+                    }
+                ]
+            }
+        ]
+
 
 # Initialize a Flask app to host the events adapter
 app = Flask(__name__)
@@ -13,133 +109,61 @@ slack_events_adapter = SlackEventAdapter(os.environ["SLACK_SIGNING_SECRET"], "/s
 
 # Initialize a Web API client
 slack_web_client = WebClient(token=os.environ['SLACK_BOT_TOKEN'])
-
-# For simplicity we'll store our app data in-memory with the following data structure.
-# onboarding_tutorials_sent = {"channel": {"user_id": OnboardingTutorial}}
-onboarding_tutorials_sent = {}
+bot_user = slack_web_client.auth_test()
+all_users = slack_web_client.users_list()
 
 
-def start_onboarding(user_id: str, channel: str):
-    # Create a new onboarding tutorial.
-    onboarding_tutorial = OnboardingTutorial(channel)
-
-    # Get the onboarding message payload
-    message = onboarding_tutorial.get_message_payload()
-
-    # Post the onboarding message in Slack
-    response = slack_web_client.chat_postMessage(**message)
-
-    # Capture the timestamp of the message we've just posted so
-    # we can use it to update the message after a user
-    # has completed an onboarding task.
-    onboarding_tutorial.timestamp = response["ts"]
-
-    # Store the message sent in onboarding_tutorials_sent
-    if channel not in onboarding_tutorials_sent:
-        onboarding_tutorials_sent[channel] = {}
-    onboarding_tutorials_sent[channel][user_id] = onboarding_tutorial
-
-
-# ================ Team Join Event =============== #
-# When the user first joins a team, the type of the event will be 'team_join'.
-# Here we'll link the onboarding_message callback to the 'team_join' event.
-@slack_events_adapter.on("team_join")
-def onboarding_message(payload):
-    """Create and send an onboarding welcome message to new users. Save the
-    time stamp of this message so we can update this message in the future.
-    """
+@slack_events_adapter.on("app_home_opened")
+def home_open(payload):
     event = payload.get("event", {})
-
-    # Get the id of the Slack user associated with the incoming event
-    user_id = event.get("user", {}).get("id")
-
-    # Open a DM with the new user.
-    response = slack_web_client.im_open(user=user_id)
-    channel = response["channel"]["id"]
-
-    # Post the onboarding message.
-    start_onboarding(user_id, channel)
-
-
-# ============= Reaction Added Events ============= #
-# When a users adds an emoji reaction to the onboarding message,
-# the type of the event will be 'reaction_added'.
-# Here we'll link the update_emoji callback to the 'reaction_added' event.
-@slack_events_adapter.on("reaction_added")
-def update_emoji(payload):
-    """Update the onboarding welcome message after receiving a "reaction_added"
-    event from Slack. Update timestamp for welcome message as well.
-    """
-    event = payload.get("event", {})
-
-    channel_id = event.get("item", {}).get("channel")
     user_id = event.get("user")
-
-    if channel_id not in onboarding_tutorials_sent:
-        return
-
-    # Get the original tutorial sent.
-    onboarding_tutorial = onboarding_tutorials_sent[channel_id][user_id]
-
-    # Mark the reaction task as completed.
-    onboarding_tutorial.reaction_task_completed = True
-
-    # Get the new message payload
-    message = onboarding_tutorial.get_message_payload()
-
-    # Post the updated message in Slack
-    updated_message = slack_web_client.chat_update(**message)
-
-    # Update the timestamp saved on the onboarding tutorial object
-    onboarding_tutorial.timestamp = updated_message["ts"]
+    print(f"Home opened from {user_id}")
+    slack_web_client.views_publish(user_id=user_id,
+                                   view=get_app_home())
 
 
-# =============== Pin Added Events ================ #
-# When a users pins a message the type of the event will be 'pin_added'.
-# Here we'll link the update_pin callback to the 'reaction_added' event.
-@slack_events_adapter.on("pin_added")
-def update_pin(payload):
-    """Update the onboarding welcome message after receiving a "pin_added"
-    event from Slack. Update timestamp for welcome message as well.
-    """
+@slack_events_adapter.on("app_mention")
+def mentioned(payload):
     event = payload.get("event", {})
-
-    channel_id = event.get("channel_id")
     user_id = event.get("user")
-
-    # Get the original tutorial sent.
-    onboarding_tutorial = onboarding_tutorials_sent[channel_id][user_id]
-
-    # Mark the pin task as completed.
-    onboarding_tutorial.pin_task_completed = True
-
-    # Get the new message payload
-    message = onboarding_tutorial.get_message_payload()
-
-    # Post the updated message in Slack
-    updated_message = slack_web_client.chat_update(**message)
-
-    # Update the timestamp saved on the onboarding tutorial object
-    onboarding_tutorial.timestamp = updated_message["ts"]
+    channel_id = event.get("channel")
+    text = event.get("text")
+    print("Mention!")
+    debug_print_msg(payload)
 
 
-# ============== Message Events ============= #
-# When a user sends a DM, the event type will be 'message'.
-# Here we'll link the message callback to the 'message' event.
 @slack_events_adapter.on("message")
 def message(payload):
-    """Display the onboarding welcome message after receiving a message
-    that contains "start".
-    """
     event = payload.get("event", {})
-
-    channel_id = event.get("channel")
     user_id = event.get("user")
+    # Disregard own message
+    if user_id == bot_user.data['user_id']:
+        return
+    channel_id = event.get("channel")
     text = event.get("text")
 
+    debug_print_msg(payload)
 
-    if text and text.lower() == "start":
-        return start_onboarding(user_id, channel_id)
+
+def debug_print_msg(payload):
+    event = payload.get("event", {})
+    user_id = event.get("user")
+    # Disregard own message
+    if user_id == bot_user.data['user_id']:
+        return
+    channel_id = event.get("channel")
+    text = event.get("text")
+
+    slack_web_client.chat_postMessage(
+        channel=channel_id,
+        blocks=get_request_block()
+#         text=f"""Hello! :tada:
+# I received >>{text}<< from {user_id} on channel {channel_id}!
+# Event type is {event.get("type")}
+#         """
+    )
+
+    # slack_web_client.views_publish(user_id=user_id, view=get_request_block())
 
 
 if __name__ == "__main__":
